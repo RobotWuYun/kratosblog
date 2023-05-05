3192d8767aef02edd8dbd742309718b33acab8b8
wuyun <wuyun@haoshuo.com>
2023-05-05 10:40:59 +0800

    add redis

--- go.mod
+++ go.mod
@@ -12,9 +12,15 @@ require (
 	google.golang.org/protobuf v1.28.1
 )
 
-require github.com/spf13/viper v1.15.0
+require (
+	github.com/redis/go-redis/v9 v9.0.4
+	github.com/spf13/viper v1.15.0
+	go.uber.org/zap v1.21.0
+)
 
 require (
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/fsnotify/fsnotify v1.6.0 // indirect
 	github.com/go-logr/logr v1.2.3 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
@@ -35,6 +41,8 @@ require (
 	github.com/subosito/gotenv v1.4.2 // indirect
 	go.opentelemetry.io/otel v1.7.0 // indirect
 	go.opentelemetry.io/otel/trace v1.7.0 // indirect
+	go.uber.org/atomic v1.9.0 // indirect
+	go.uber.org/multierr v1.8.0 // indirect
 	golang.org/x/mod v0.6.0-dev.0.20220419223038-86c51ed26bb4 // indirect
 	golang.org/x/net v0.4.0 // indirect
 	golang.org/x/sync v0.1.0 // indirect
--- internal/data/data.go
+++ internal/data/data.go
@@ -1,10 +1,13 @@
 package data
 
 import (
+	"go.uber.org/zap"
 	"kratosblog/internal/conf"
 
 	"github.com/go-kratos/kratos/v2/log"
 	"github.com/google/wire"
+
+	"github.com/redis/go-redis/v9"
 )
 
 // ProviderSet is data providers.
@@ -19,14 +22,68 @@ var ProviderSet = wire.NewSet(
 // Data .
 type Data struct {
 	conf *conf.Bootstrap
+	Log  *log.Helper
+
+	Redis *redis.Client
 }
 
+type initFn func(*conf.Bootstrap) (cleanup func(), err error)
+
 // NewData .
-func NewData(c *conf.Bootstrap, logger log.Logger) (*Data, func(), error) {
-	cleanup := func() {
-		log.NewHelper(logger).Info("closing the data resources")
-	}
-	return &Data{
+func NewData(c *conf.Bootstrap, logger log.Logger) (data *Data, cleanup func(), err error) {
+	data = &Data{
 		conf: c,
-	}, cleanup, nil
+		Log:  log.NewHelper(logger),
+	}
+
+	closeFns := make([]func(), 0)
+
+	initFns := []initFn{
+		data.initCache,
+		//data.initMongo,
+		//data.initMQ,
+		//data.initMQJob,
+		//data.initES, // 放在初始化缓存之后
+
+	}
+
+	for _, initFn := range initFns {
+		var closeFn func()
+		closeFn, err = initFn(c)
+		if err != nil {
+			return
+		}
+		closeFns = append(closeFns, closeFn)
+	}
+
+	// 初始化清理函数
+	cleanup = func() {
+		log.Info("closing the data resources")
+		for _, fn := range closeFns {
+			if fn != nil {
+				fn()
+			}
+		}
+	}
+	return
+}
+
+func (d *Data) initCache(c *conf.Bootstrap) (cleanup func(), err error) {
+	redisConf := c.GetData().GetRedis()
+	d.Redis = redis.NewClient(&redis.Options{
+		Addr:         redisConf.GetAddr(),
+		Password:     redisConf.GetPassword(), // 没有密码，默认值
+		DB:           int(redisConf.GetDb()),  // 默认DB 0
+		ReadTimeout:  redisConf.GetReadTimeout().AsDuration(),
+		WriteTimeout: redisConf.GetWriteTimeout().AsDuration(),
+	})
+
+	cleanup = func() {
+		d.Log.Info("closing the redis resources")
+		err = d.Redis.Close()
+		if err != nil {
+			d.Log.Info("closing the redis resources err.", zap.Error(err))
+		}
+	}
+	return
 }
